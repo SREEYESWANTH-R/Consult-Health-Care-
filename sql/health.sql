0b9cfe0a42fb79750a424a1461287a410eff70d6
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2024-09-19 18:35:47 +0530

    Updated sql schema

--- sql/health.sql
+++ sql/health.sql
@@ -3,15 +3,18 @@ CREATE TABLE signup (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    active BOOLEAN DEFAULT FALSE
 );
 select * from signup;
 
-CREATE TABLE session (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL
+CREATE TABLE profile (
+    user_id INT PRIMARY KEY,
+    mobile VARCHAR(15),
+    address VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES signup(id) ON DELETE CASCADE
 );
-SELECT * FROM session;
+SELECT * From profile;
 
 CREATE TABLE adminLog(
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,15 +27,17 @@ INSERT INTO adminLog (adminname, Password) VALUES
 
 SELECT * FROM adminLog;
 
+
 CREATE TABLE appointment (
 	id INT auto_increment Primary Key,
     name VARCHAR(100) NOT NULL,
+    doctor VARCHAR(100) NOT NULL,
     age INT NOT NULL,
     gender CHAR(1) NOT NULL,
     mobNum VARCHAR(15) NOT NULL,
-    address VARCHAR(255) NOT NULL
+    address VARCHAR(255) NOT NULL,
+    date DATE NOT NULL
 );
-
 SELECT * FROM appointment;
 
 CREATE TABLE doctor(
@@ -44,5 +49,28 @@ CREATE TABLE doctor(
     expertise VARCHAR(100) NOT NULL,
     image BLOB
 );
-
 SELECT * FROM doctor;
+
+CREATE TABLE Drugs (
+    id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    brand VARCHAR(255),
+    generic VARCHAR(255),
+    dosage_form VARCHAR(255),
+    strength VARCHAR(255),
+    route VARCHAR(255),
+    description TEXT,
+    cost DECIMAL(10, 2) NOT NULL
+);
+
+select * From Drugs;
+
+CREATE Table Cart(
+	 prescription_id INT AUTO_INCREMENT PRIMARY KEY,
+     drug_id VARCHAR(10) NOT NULL,
+     quantity INT NOT NULL,
+	 cost DECIMAL(10, 2) NOT NULL,
+     FOREIGN KEY (apppoinment_id) references appointment(id) ON DELETE cascade,
+     FOREIGN KEY (drug_id) references Drugs(id) ON delete cascade
+);
+select * from Cart;
